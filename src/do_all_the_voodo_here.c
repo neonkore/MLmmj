1c3f1d51e3fa8615b77cbc238498c7618bbc35b8
mmj <none@none>
2004-04-22 23:00:27 +1000

    Add GPL info in top of files where it was missing, and remove a file that was committed by mistake

--- src/do_all_the_voodo_here.c
+++ src/do_all_the_voodo_here.c
@@ -1,3 +1,11 @@
+/* Copyright (C) 2004 Mads Martin Joergensen <mmj at mmj.dk>
+ *
+ * $Id$
+ *
+ * This file is redistributable under version 2 of the GNU General
+ * Public License as described at http://www.gnu.org/licenses/gpl.txt
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- src/print-version.c
+++ src/print-version.c
@@ -1,3 +1,11 @@
+/* Copyright (C) 2003, 2004 Mads Martin Joergensen <mmj at mmj.dk>
+ *
+ * $Id$
+ *
+ * This file is redistributable under version 2 of the GNU General
+ * Public License as described at http://www.gnu.org/licenses/gpl.txt
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <strings.h>
@@ -6,7 +14,7 @@
 
 void print_version(const char *prg)
 {
-	char *lastslashindex = rindex(prg, '/');
+	char *lastslashindex = strrchr(prg, '/');
 	
 	if(!lastslashindex)
 		lastslashindex = (char *)prg;
