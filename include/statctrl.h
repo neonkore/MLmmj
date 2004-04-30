7c03ed2062ea1958fba535fcdde2c251e6ffd406
mmj <none@none>
2004-04-30 18:57:33 +1000

    This is new function to quickly stat a control of the list. Example:
    int moderatedlist = statctrl(listdir, "moderated);
    
    Then it'll return true/false according to whether a file
    listdir/control/moderated exists.

---
+++ include/statctrl.h
@@ -0,0 +1,14 @@
+/* Copyright (C) 2004 Mads Martin Joergensen <mmj at mmj.dk>
+ *
+ * $Id$
+ *
+ * This file is redistributable under version 2 of the GNU General
+ * Public License as described at http://www.gnu.org/licenses/gpl.txt
+ */
+
+#ifndef STATCTRL_H
+#define STATCTRL_H
+
+int statctrl(const char *listdir, const char *ctrlstr);
+
+#endif /* STATCTRL_H */
