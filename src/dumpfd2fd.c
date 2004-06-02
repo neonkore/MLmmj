bb5c37f808866baec34da90262706c60b8a4399a
mmj <none@none>
2004-06-03 08:47:20 +1000

    dumpfd2fd

---
+++ src/dumpfd2fd.c
@@ -0,0 +1,36 @@
+/* Copyright (C) 2004 Mads Martin Joergensen <mmj at mmj dot dk>
+ *
+ * $Id$
+ *
+ * This file is redistributable under version 2 of the GNU General
+ * Public License as described at http://www.gnu.org/licenses/gpl.txt
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include "wrappers.h"
+
+#define DUMPBUF 1024
+
+int dumpfd2fd(int infd, int outfd)
+{
+	size_t n;
+	char buf[DUMPBUF];
+
+	while((n = read(infd, &buf, sizeof(buf))) != 0) {
+		if(n < 0) {
+			if(errno == EINTR)
+				continue;
+			else
+				return errno;
+		}
+		if(writen(outfd, &buf, n) < 0)
+			return errno;
+	}
+	
+	return 0;
+}
