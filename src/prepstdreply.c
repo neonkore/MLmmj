32ce36f3ab17274778412409511d3231c543fb87
mmj <none@none>
2004-06-07 23:54:49 +1000

    Add GPL header

--- src/prepstdreply.c
+++ src/prepstdreply.c
@@ -1,3 +1,11 @@
+/* Copyright (C) 2004 Mads Martin Joergensen <mmj at mmj.dk>
+ *
+ * $Id$
+ *
+ * This file is redistributable under version 2 of the GNU General
+ * Public License as described at http://www.gnu.org/licenses/gpl.txt
+ */
+
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
