419713b66eec86015dec78b944e070d3bec427c2
mmj <none@none>
2004-06-08 20:17:16 +1000

    openrandexclrw added

--- include/wrappers.h
+++ include/wrappers.h
@@ -14,5 +14,6 @@
 ssize_t writen(int fd, const void *vptr, size_t n);
 int random_int(void);
 int dumpfd2fd(int infd, int outfd);
+int openrandexclrw(const char *dir, const char *prefix, mode_t mode);
 
 #endif /* WRAPPERS_H */
