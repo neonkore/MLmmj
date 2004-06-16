ac3462ac51ec98e88bd4f78713531001c569ef11
mmj <none@none>
2004-06-17 07:11:55 +1000

    Goodbye GPL license, Welcome MIT

--- src/mlmmj-send.c
+++ src/mlmmj-send.c
@@ -2,8 +2,23 @@
  *
  * $Id$
  *
- * This file is redistributable under version 2 of the GNU General
- * Public License as described at http://www.gnu.org/licenses/gpl.txt
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
  */
 
 #include <stdio.h>
@@ -171,8 +186,8 @@ int send_mail(int sockfd, const char *from, const char *to,
 	if(reply) {
 		write_rset(sockfd);
 		checkwait_smtpreply(sockfd, MLMMJ_RSET);
-		if(mlmmjbounce && (reply[1] == '5') && ((reply[0] == '4') ||
-					(reply[0] == '5'))) {
+		if(mlmmjbounce && ((reply[0] == '4') || (reply[0] == '5'))
+				&& (reply[1] == '5')) {
 			free(reply);
 			return bouncemail(listdir, mlmmjbounce, from);
 		} else {
