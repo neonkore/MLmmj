fc1e040881e0d4ec1218777820c15d3d23dfe440
mmj <none@none>
2004-11-18 00:03:56 +1100

    Add newline after generic subject

--- src/prepstdreply.c
+++ src/prepstdreply.c
@@ -167,7 +167,7 @@ char *prepstdreply(const char *listdir, const char *filename, const char *from,
 	if(strncasecmp(tmp, "Subject:", 8) != 0) {
 		log_error(LOG_ARGS, "No Subject in listtexts. Using "
 				"standard subject");
-		subject = mystrdup("mlmmj administrativa");
+		subject = mystrdup("mlmmj administrativa\n");
 	} else
 		subject = substitute(tmp, listaddr, tokencount, data);
 
