783bae50eef46ad30d0305990bff455c0775cb1b
mmj <none@none>
2005-05-12 17:14:33 +1000

    remove superflous check

--- src/checkwait_smtpreply.c
+++ src/checkwait_smtpreply.c
@@ -43,7 +43,7 @@ char *checkwait_smtpreply(int sockfd, int replytype)
 
 	do {
 		len += read(sockfd, (smtpreply+len), RFC_REPLY_SIZE - len);
-	} while(smtpreply[len - 1] != '\n' || len >= RFC_REPLY_SIZE);
+	} while(smtpreply[len - 1] != '\n');
 
 	smtpreply[len] = '\0';
 #if 0
