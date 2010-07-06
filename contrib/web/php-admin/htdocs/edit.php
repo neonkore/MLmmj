14e500290622a43baa1fcf61ae646e43d5acb9aa
Ben Schmidt <none@none>
2010-07-07 01:05:36 +1000

    Validate input in php-admin to avoid altering arbitrary files (Florian Streibelt, Morten Shearman Kirkegaard)

--- contrib/web/php-admin/htdocs/edit.php
+++ contrib/web/php-admin/htdocs/edit.php
@@ -104,6 +104,15 @@ $list = $HTTP_GET_VARS["list"];
 if(!isset($list))
 die("no list specified");
 
+if (strchr($list, "/") !== false)
+die("slash in list name");
+
+if ($list == ".")
+die("list name is dot");
+
+if ($list == "..")
+die("list name is dot-dot");
+
 if(!is_dir($topdir."/".$list))
 die("non-existent list");
 
--- contrib/web/php-admin/htdocs/save.php
+++ contrib/web/php-admin/htdocs/save.php
@@ -79,6 +79,15 @@ $list = $HTTP_POST_VARS["list"];
 if(!isset($list))
 die("no list specified");
 
+if (strchr($list, "/") !== false)
+die("slash in list name");
+
+if ($list == ".")
+die("list name is dot");
+
+if ($list == "..")
+die("list name is dot-dot");
+
 if(!is_dir($topdir."/".$list))
 die("non-existent list");
 
